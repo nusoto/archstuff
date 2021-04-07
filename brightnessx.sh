711b4483c35109625727c7dbfc46552f475d5182
nusoto <68717290+nusoto@users.noreply.github.com>
2021-04-07 17:28:01 -0300

    Add files via upload

--- brightnessx.sh
+++ brightnessx.sh
@@ -1,13 +1,25 @@
-#!/bin/zsh
+#!/bin/sh
 
-vared -p 'Screen Brightness value: ' -c brightness
+if [ -z "$1" ]
+then
+	vared -p 'Screen Brightness value: ' -c brightness
+elif echo "$1" | grep -qE '^[0-9]+$'
+then
+	brightness=$1
+else
+	exit 0
+fi
 
-if [ $brightness -gt 4000 ]; then
+
+if [ $brightness -gt 4000 ]
+then
 	brightness=4000;
-elif [ $brightness -lt 100 ]; then
+elif [ $brightness -lt 100 ]
+then
 	brightness=100;
 fi
 
-if [ $brightness -le 4000 ] && [ $brightness -ge 100 ]; then
+if [ $brightness -le 4000 ] && [ $brightness -ge 100 ]
+then
 	echo $brightness | sudo tee /sys/class/backlight/intel_backlight/brightness
 fi
